ed8c563b5560f8101d34e99ed9394919a026465a
K Rishabh <90962594+k-rishabh6172@users.noreply.github.com>
2023-12-20 23:11:30 +0530

    Create xercise_50_Moveallnegativeelementstoonesideofthearray.cpp

---
+++ xercise_50_Moveallnegativeelementstoonesideofthearray.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+ void moveNegativesToEnd(int* arr,int n)
+    {
+        int temp[n] ;
+        int j = 0;
+         
+        for (int i = 0; i < n; i++)
+            if (arr[i] >= 0)
+                temp[j++] = arr[i];
+                
+        if (j == n || j == 0)
+            return;
+            
+        for (int i = 0; i < n; i++)
+            if (arr[i] < 0)
+                temp[j++] = arr[i];
+                
+        for (int i = 0; i < n; i++) 
+            arr[i] = temp[i];
+            
+        
+        
+    }
