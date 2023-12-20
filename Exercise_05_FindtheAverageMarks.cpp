16f54bcd7b2384a1bb68eca848715de5ebbdcaf0
K Rishabh <90962594+k-rishabh6172@users.noreply.github.com>
2023-12-20 22:05:42 +0530

    Create Exercise_05_FindtheAverageMarks.cpp

---
+++ Exercise_05_FindtheAverageMarks.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+void mainFn() {
+     float a,b,c,d,e;
+    cin >>a >> b >> c >> d >> e ;
+    float t=a+b+c+d+e ;
+    float ans=t/5;
+    cout << fixed << setprecision(2) << ans ;
+        
+}
