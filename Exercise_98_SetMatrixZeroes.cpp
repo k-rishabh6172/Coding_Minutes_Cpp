3723ac13f777fcedd0d5bc5224af014d4daa3354
K Rishabh <90962594+k-rishabh6172@users.noreply.github.com>
2023-12-21 00:13:45 +0530

    Create Exercise_98_SetMatrixZeroes.cpp

---
+++ Exercise_98_SetMatrixZeroes.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ void setZeroes(vector<vector<int>> matrix) {
+         bool row = false, col = false;
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                if(matrix[i][j] == 0) {
+                    if(i == 0) row = true;
+                    if(j == 0) col = true;
+                    matrix[0][j] = matrix[i][0] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < matrix.size(); i++){
+            for(int j = 1; j < matrix[0].size(); j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+        if(col){
+            for(int i = 0; i < matrix.size(); i++) matrix[i][0] = 0;
+        }
+        if(row){
+            for(int j = 0; j < matrix[0].size(); j++) matrix[0][j] = 0;
+        }
+        
+        for (auto row : matrix)
+        {
+            for (auto element : row)
+                cout << element << " ";
+        }
+        
+        
+    }
