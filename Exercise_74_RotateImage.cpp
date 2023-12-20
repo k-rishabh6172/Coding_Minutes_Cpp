fa8a17f19310c16b7c27eccd11d738abf394a6f3
K Rishabh <90962594+k-rishabh6172@users.noreply.github.com>
2023-12-20 23:42:55 +0530

    Create Exercise_74_RotateImage.cpp

---
+++ Exercise_74_RotateImage.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+void transpose(int matrix[][50],int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+    }
+    
+     void reflect(int matrix[][50],int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+void rotate(int matrix[][50],int n) {
+        transpose(matrix, n);
+        reflect(matrix,n);
+    }
