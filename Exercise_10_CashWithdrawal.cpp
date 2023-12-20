b517db3cbda31ad9d0117cc51c3964fd27cfd498
K Rishabh <90962594+k-rishabh6172@users.noreply.github.com>
2023-12-20 22:11:15 +0530

    Create Exercise_10_CashWithdrawal.cpp

---
+++ Exercise_10_CashWithdrawal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+void mainFn() {
+   int x,y ; cin >> x >> y ;
+    
+    float c = 0.9 * x ;
+    
+    if(y>c) cout << "no" ;
+    else cout << "yes" ;
+
+}
